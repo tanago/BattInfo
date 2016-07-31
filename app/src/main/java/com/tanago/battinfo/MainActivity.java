ce91a232822baa34b0bcfe2e39037c35aef70471
tanago <alphasix@abv.bg>
2016-07-31 11:42:18 +0300

    Basic Functionality

--- app/src/main/java/com/tanago/battinfo/MainActivity.java
+++ app/src/main/java/com/tanago/battinfo/MainActivity.java
@@ -2,6 +2,11 @@ package com.tanago.battinfo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +18,20 @@ public class MainActivity extends AppCompatActivity {
 
     private void test(){
         int i = 0;
+    }
+
+    private void getCurrent(){
+        TextView textView = (TextView) findViewById(R.id.fieldCurrent);
+        String info="Not Found!";
+        try{
+            FileReader file = new FileReader("/sys/class/power_supply/battery/current_now");
+            BufferedReader bfrd = new BufferedReader(file);
+            info=bfrd.readLine();
+        }
+        catch(IOException e){
+            System.out.println("Error while reading a file.");
+        }
+
+        textView.setText(info);
     }
 }
