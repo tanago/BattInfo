757280df4a61443897b3791e0f353af6bac9ae90
tanago <alphasix@abv.bg>
2016-08-01 12:28:49 +0300

    Basic functionality working on Xperia phones!

--- app/src/main/java/com/tanago/battinfo/MainActivity.java
+++ app/src/main/java/com/tanago/battinfo/MainActivity.java
@@ -2,9 +2,12 @@ package com.tanago.battinfo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -16,22 +19,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private void test(){
-        int i = 0;
+    private static String reformat(String prefix, String suffix){
+        return prefix.substring(0, prefix.length()-3) + " "+ suffix;
     }
 
-    private void getCurrent(){
-        TextView textView = (TextView) findViewById(R.id.fieldCurrent);
-        String info="Not Found!";
+    private void getStatus(){
+        TextView tvS = (TextView) findViewById(R.id.fieldStatus);
+        String info="";
         try{
-            FileReader file = new FileReader("/sys/class/power_supply/battery/current_now");
+            FileReader file = new FileReader("/sys/class/power_supply/battery/status");
             BufferedReader bfrd = new BufferedReader(file);
             info=bfrd.readLine();
         }
         catch(IOException e){
             System.out.println("Error while reading a file.");
         }
-
-        textView.setText(info);
+        finally {
+            tvS.setText(info);
+        }
     }
+    private void getCurrent(){
+        TextView tvC= (TextView) findViewById(R.id.fieldCurrent);
+        String info="";
+        try{
+            FileReader file = new FileReader("/sys/class/power_supply/battery/current_now");
+            BufferedReader bfrd = new BufferedReader(file);
+            info=bfrd.readLine();
+            info=reformat(info, "mAh");
+        }
+        catch(IOException e){
+            System.out.println("Error while reading a file.");
+        }
+        finally {
+            tvC.setText(info);
+        }
+    }
+
+    public void buttonOnClick(View v){
+        File file = new File("/sys/class/power_supply/battery/");
+        Button button = (Button) v;
+        if(!file.exists()) {
+            button.setText("Device unsupported :(");
+            return;
+        }
+        getStatus();
+        getCurrent();
+
+
+    }
+
 }
