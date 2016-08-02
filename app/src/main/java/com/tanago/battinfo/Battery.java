38d93009e08112c18b3a304a61904126839a2b2d
tanago <alphasix@abv.bg>
2016-08-02 14:02:47 +0300

    Basic functionality working on Xperia phones!

--- app/src/main/java/com/tanago/battinfo/Battery.java
+++ app/src/main/java/com/tanago/battinfo/Battery.java
@@ -27,6 +27,7 @@ class Battery {
         }
         return data;
     }
+
     protected String getCurrent(){
         String data="missing";
         try{
@@ -41,4 +42,64 @@ class Battery {
         return data;
     }
 
+    protected String getPercentage(){
+        String data="missing";
+        try{
+            FileReader file = new FileReader("/sys/class/power_supply/battery/capacity");
+            BufferedReader bfrd = new BufferedReader(file);
+            data=bfrd.readLine();
+            data=reformat(data, "%");
+        }
+        catch(IOException e){
+            System.out.println("Error while reading \"capacity\" file.");
+        }
+        return data;
+    }
+
+//         protected String getWear(){
+//
+//        //TODO x=100-(charge_full/charge_full_design)*100 (if x<0  x = 0 )
+//        String data="missing";
+//        try{
+//            FileReader file = new FileReader("/sys/class/power_supply/battery/capacity");
+//            BufferedReader bfrd = new BufferedReader(file);
+//            data=bfrd.readLine();
+//            data=reformat(data, "mAh");
+//        }
+//        catch(IOException e){
+//            System.out.println("Error while reading \"current\" file.");
+//        }
+//        return data;
+//    }
+
+
+    protected String getTemp(){
+        String data="missing";
+        try{
+            FileReader file = new FileReader("/sys/class/power_supply/battery/temp");
+            BufferedReader bfrd = new BufferedReader(file);
+            data=bfrd.readLine();
+            data=reformat(data, "mAh");
+        }
+        catch(IOException e){
+            System.out.println("Error while reading \"temp\" file.");
+        }
+        return data;
+    }
+
+//    protected String getTempScale(){
+//
+//        //TODO ( temp
+//        String data="missing";
+//        try{
+//            FileReader file = new FileReader("/sys/class/power_supply/battery/capacity");
+//            BufferedReader bfrd = new BufferedReader(file);
+//            data=bfrd.readLine();
+//            data=reformat(data, "mAh");
+//        }
+//        catch(IOException e){
+//            System.out.println("Error while reading \"current\" file.");
+//        }
+//        return data;
+//    }
 }
