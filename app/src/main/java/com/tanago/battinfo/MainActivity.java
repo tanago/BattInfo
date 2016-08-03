21f4bcddad3723880a5b65f061ab262a0cc408db
ele <elekeloke@hotmail.com>
2016-08-03 18:39:37 +0200

    Missing files checks

--- app/src/main/java/com/tanago/battinfo/MainActivity.java
+++ app/src/main/java/com/tanago/battinfo/MainActivity.java
@@ -1,10 +1,15 @@
 package com.tanago.battinfo;
 
-import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,11 +27,19 @@ public class MainActivity extends AppCompatActivity {
         if (field!=null) field.setText(s);
 
     }
-    public void buttonOnClick(View v) {
+    public void buttonOnClick(View v) throws IOException{
         fillField(R.id.fieldStatus, battery.getStatus());
         fillField(R.id.fieldCurrent, battery.getCurrent());
         fillField(R.id.fieldPercent, battery.getPercentage());
         fillField(R.id.fieldTemp, battery.getTemp());
         fillField(R.id.fieldWear, battery.getWear());
+
+        File files = new File("/sys/class/power_supply/battery/");
+        BufferedReader bfr;
+        for ( File file : files.listFiles()){
+            if(file.isDirectory()) continue;
+            bfr = new BufferedReader(new FileReader(file));
+            System.err.println(file.getName() + "\t" + bfr.readLine() );
+        }
     }
 }
