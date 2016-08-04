93a0e3f9653d34ea40e08b784f5acc79c3c785c6
tanago <alphasix@abv.bg>
2016-08-04 23:52:12 +0300

    implements Runnable

--- app/src/main/java/com/tanago/battinfo/MainActivity.java
+++ app/src/main/java/com/tanago/battinfo/MainActivity.java
@@ -4,44 +4,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements Runnable{
 
     Battery battery = new Battery();
     TextView field;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    private void fillField(int i, String s){
-        field = (TextView) findViewById(i);
-        if (field!=null) field.setText(s);
-
-    }
-    public void buttonOnClick(View v) throws IOException{
+    public void run(){
         fillField(R.id.fieldStatus, battery.getStatus());
         fillField(R.id.fieldCurrent, battery.getCurrent());
         fillField(R.id.fieldPercent, battery.getPercentage());
         fillField(R.id.fieldTemp, battery.getTemp());
         fillField(R.id.fieldWear, battery.getWear());
         fillField(R.id.fieldVoltage, battery.getVolt());
+    }
 
-        File files = new File("/sys/class/power_supply/battery/");
-        BufferedReader bfr;
-        for ( File file : files.listFiles()){
-            if(file.isDirectory()) continue;
 
-            bfr = new BufferedReader(new FileReader(file));
-            System.err.println(file.getName() + "\t" + bfr.readLine() );
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        run();
+    }
+
+    protected void fillField(int i, String s){
+        field = (TextView) findViewById(i);
+        if (field!=null) field.setText(s);
+
+    }
+
+    public void buttonOnClick(View v) throws IOException{
+
+
     }
 }
